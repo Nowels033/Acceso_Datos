f8334f53c4920b12057f63cabe6975fb07a7e628
Nowels033 <noeldomse@gmail.com>
2024-10-18 15:22:32 +0200

    NetBeans

---
+++ src/main/java/ejercicio_Gestion_De_Jugadores/FileManager.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ejercicio_Gestion_De_Jugadores;
+
+/**
+ *
+ * @author ALUMNO CCC - TARDE
+ */
+public class FileManager {
+    
+//    public static void main(String[] args) {
+//        
+//    }
+    
+    
+    
+}
