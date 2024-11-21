0226b6d62fe8fef0541146557ee653ce3142a66a
Nowels033 <noeldomse@gmail.com>
2024-11-21 19:41:59 +0100

    21/11/2024 GestionDeUsuariosApp

--- src/main/java/gestionDeUsuarios/GestionDeUsuariosApp.java
+++ src/main/java/gestionDeUsuarios/GestionDeUsuariosApp.java
@@ -2,21 +2,8 @@ package gestionDeUsuarios;
 
 public class GestionDeUsuariosApp {
     public static void main(String[] args) {
-
-        menu();
-
-    }
-    public static void menu() {
-
-
-
         UserDTO user = new UserDTO();
-
-
-       // user.crearUsuario();
         user.login();
-
-
     }
 
 }
